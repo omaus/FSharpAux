84c829f301c9a928a9391cd32ee3e67cbdd19bbd
omaus <omaus@bio.uni-kl.de>
2022-12-19 18:02:49 +0100

    (WIP) Add tests

--- tests/FSharpAux.Tests/UriTests.fs
+++ tests/FSharpAux.Tests/UriTests.fs
@@ -1,2 +1,9 @@
 ﻿module UriTests
 
+open FSharpAux
+open Expecto
+open System
+
+let testUri1 = Uri("localhost:1000")
+let testUri1' = Uri("localhost:1000/")
+let resultUri1 = Uri(testUri1', "ping")
