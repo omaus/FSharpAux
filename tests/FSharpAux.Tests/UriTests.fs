0eb02df85f29753d33c861e7e5423d6e7b330144
omaus <omaus@bio.uni-kl.de>
2022-12-20 18:00:19 +0100

    (WIP) Add Uri tests

--- tests/FSharpAux.Tests/UriTests.fs
+++ tests/FSharpAux.Tests/UriTests.fs
@@ -6,4 +6,17 @@ open System
 
 let testUri1 = Uri("localhost:1000")
 let testUri1' = Uri("localhost:1000/")
-let resultUri1 = Uri(testUri1', "ping")
+let resultUri1a = Uri(testUri1', "ping")
+let resultUri2a = Uri(Uri(testUri1', "ping/"), "pong")
+
+let resultUri1b = Uri.combine [|"localhost:1000"; |]
+
+[<Tests>]
+let uriTests =
+    testList "UriTests" [
+        testList "Uri.combine" [
+            testCase "concatinates Uris correctly" (fun _ ->
+                Expect.isTrue
+            )
+        ]
+    ]
