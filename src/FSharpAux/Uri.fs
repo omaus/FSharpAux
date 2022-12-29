7a806c3bb30bdd6758f3953172dee1e9ca40b529
omaus <oliver.maus@eclipso.de>
2022-12-29 09:38:30 +0100

    Update & expand functionality of Uri module

--- src/FSharpAux/Uri.fs
+++ src/FSharpAux/Uri.fs
@@ -3,20 +3,28 @@
 open FSharpAux
 open System
 
-/// Combines an array of URIs into one.
-let combine (uris : string []) =
-    if String.isNullOrEmpty uris[0] then raise (UriFormatException())
+
+/// Creates a single URI out of a collection of strings.
+let ofStrings (uris : #seq<string>) =
+    if String.isNullOrEmpty (Seq.item 0 uris) then raise (UriFormatException())
     let revisedUris =
         uris
-        |> Array.mapi (
+        |> Seq.mapi (
             fun i u ->
                 if String.isNullOrEmpty u then u
-                elif String.last u <> '/' && i < uris.Length - 1 then u + "/"
+                elif String.last u <> '/' && i < Seq.length uris - 1 then u + "/"
                 else u
         )
     let rec loop baseUri i =
-        if i < uris.Length - 1 then
-            let newUri = Uri(baseUri, revisedUris[i])
+        if i < Seq.length uris - 1 then
+            let newUri = Uri(baseUri, Seq.item i revisedUris)
             loop newUri (i + 1)
-        else Uri(baseUri, revisedUris[i])
-    loop (Uri revisedUris[0]) 0
+        else Uri(baseUri, Seq.item i revisedUris)
+    loop (Uri (Seq.head revisedUris)) 0
+    
+/// Combines a collection of URIs into one.
+let combine (uris : #seq<Uri>) =
+    uris
+    |> Seq.fold (
+        fun acc uri -> Uri(acc, uri)
+    ) (Seq.head uris)
